174059779814ecd7eea963f9060c185ce8ecb7ed
kevin <946237542@qq.com>
2020-07-09 14:31:09 +0800

    xx

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div>sdkljflksljfl</div>
     <h3>啊啊啊啊啊</h3>
     <div>使用打包后的文件运行</div>
+    <p>ppppppp</p>
     <ul>
       <li v-for="(user) in users" :key="user.id">{{user.id}}:{{user.name}}</li>
     </ul>
